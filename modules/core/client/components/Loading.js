486567c734c2530fb62dea6661960c24b43055cc
Mikael Korpela <mikael@ihminen.org>
2019-05-25 14:28:02 +0300

    i18n update

--- modules/core/client/components/Loading.js
+++ modules/core/client/components/Loading.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import '@/config/client/i18n';
-import { withNamespaces } from 'react-i18next';
+import { withTranslation } from '@/modules/core/client/utils/i18n-angular-load';
 
 /**
  * I'm just a little loading info.
  */
-export default withNamespaces('core')(({ t }) => (
+export default withTranslation('core')(({ t }) => (
   <div>{t('Wait a moment...')}</div>
 ));
